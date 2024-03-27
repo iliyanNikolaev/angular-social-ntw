759964019152b41d5aa95566ba6fec1c4e364f57
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-27 10:44:58 +0200

    Back End - Post Entity Logic => scaffold

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,6 +1,63 @@
+const { getLastPosts, getLastPostsByUserId, toggleLikeByPostId, createPost, editPostById, deletePostById } = require('../services/postService');
 const { errorParser } = require('../utils/errorParser');
 
 const postController = require('express').Router();
 
-
+postController.get('/', async (req, res) => {
+    try {
+        const posts = await getLastPosts();
+        res.status(200).json(users);
+    } catch (err) {
+        const errors = errorParser(err);
+        res.status(400).json({ errors });
+    }
+});
+postController.get('/byUser/:userId', async (req, res) => {
+    try {
+        const posts = await getLastPostsByUserId(req.params.userId);
+        res.status(200).json(posts);
+    } catch (err) {
+        const errors = errorParser(err);
+        res.status(400).json({ errors });
+    }
+});
+postController.post('/like/:id', async (req, res) => {
+    try {
+        const posts = await toggleLikeByPostId(req.params.id);
+        res.status(200).json(posts);
+    } catch (err) {
+        const errors = errorParser(err);
+        res.status(400).json({ errors });
+    }
+});
+postController.post('/create', async (req, res) => {
+    try {
+        // createPostDto
+        const posts = await createPost(req.body);
+        res.status(200).json(posts);
+    } catch (err) {
+        const errors = errorParser(err);
+        res.status(400).json({ errors });
+    }
+});
+postController.put('/edit/:id', async (req, res) => {
+    try {
+        // createPostDto
+        const posts = await editPostById(req.params.id);
+        res.status(200).json(posts);
+    } catch (err) {
+        const errors = errorParser(err);
+        res.status(400).json({ errors });
+    }
+});
+postController.delete('/delete/:id', async (req, res) => {
+    try {
+        // createPostDto
+        const posts = await deletePostById(req.params.id);
+        res.status(200).json(posts);
+    } catch (err) {
+        const errors = errorParser(err);
+        res.status(400).json({ errors });
+    }
+});
 module.exports = postController;
--- server/services/postService.js
+++ server/services/postService.js
@@ -1,5 +1,33 @@
 const Post = require('../models/Post');
 
+async function getLastPosts() {
+    return ["last 5 posts with populated comments and owner or error"]
+}
+
+async function getLastPostsByUserId(id) {
+    return ["last 5 posts or error"]
+}
+async function toggleLikeByPostId(id) {
+    return ["ok or error"]
+}
+
+async function createPost(postData) {
+    return ["created post or error"]
+}
+
+async function editPostById(id) {
+    return ["edited post or error"]
+}
+
+async function deletePostById(id) {
+    return ["ok or error"]
+}
+
 module.exports = {
-    
+    getLastPosts,
+    getLastPostsByUserId,
+    toggleLikeByPostId,
+    createPost,
+    editPostById,
+    deletePostById
 }
