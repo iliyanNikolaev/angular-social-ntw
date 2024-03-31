7e3e9da831e8ce25e267364a682e544055ada1c2
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-03-31 11:28:11 +0300

    Angular - static home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostItemComponent } from './post-item/post-item.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, CreatePostComponent, PostListComponent, PostItemComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    CreatePostComponent,
+    PostListComponent
   ]
 })
 export class CoreModule { }
