4547c8efaaa815851ef1073ee34ea60fbdf30623
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-04-02 09:10:09 +0300

    Angular - upload img to Cloudinary -> service fn

--- client/src/app/cloudinary.service.ts
+++ client/src/app/cloudinary.service.ts
@@ -1,10 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CloudinaryService {
+  private cloudinaryUploadUrl = 'https://api.cloudinary.com/v1_1/dwq3ysahj/image/upload';
+  private cloudinaryUploadPreset = 'wunldj1y';
 
   constructor(private http: HttpClient) { }
+
+  uploadImage(imageFile: File): Observable<string> {
+    const formData = new FormData();
+    formData.append('file', imageFile);
+    formData.append('upload_preset', this.cloudinaryUploadPreset);
+
+    return this.http.post<any>(this.cloudinaryUploadUrl, formData).pipe(
+      map(response => response.url)
+    );
+  }
 }
