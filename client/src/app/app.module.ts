e39f8fc56911f1ea3607ec06b0ca798b936d1828
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-04-02 08:54:44 +0300

    Angular - services -> scaffolds

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -26,7 +26,8 @@ import { PostDetailsComponent } from './post-details/post-details.component';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
