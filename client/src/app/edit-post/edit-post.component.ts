5366193fe2178f6fbaed0d70beb0cdfefb379dd2
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-04-06 08:36:14 +0300

    Angular - Edit Post

--- client/src/app/edit-post/edit-post.component.ts
+++ client/src/app/edit-post/edit-post.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../user.service';
-import { CloudinaryService } from '../cloudinary.service';
-import { AuthService } from '../auth.service';
-import { AuthData } from '../types/AuthData';
 import { Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { Post } from '../types/Post';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-edit-post',
@@ -12,19 +11,46 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./edit-post.component.css']
 })
 export class EditPostComponent implements OnInit {
-  authData: AuthData | null = null;
   private authDataSubscription: Subscription | undefined;
-  constructor(private router: Router, private sUser: UserService, private sCloudinary: CloudinaryService, private sAuth: AuthService) {
+  currPost: Post | null = null;
+  paramId: string = '';
+  isLoading: boolean = false;
+
+  constructor(private router: Router, private route: ActivatedRoute, private sPost: PostService) {
 
   }
 
   ngOnInit(): void {
-    this.getAuthData();
-  }
-  
-  getAuthData() {
-    this.authDataSubscription = this.sAuth.getAuthDataObservable().subscribe((data) => {
-      this.authData = data;
+    this.route.params.subscribe(params => {
+      this.paramId = params['id'];
+      this.sPost.getPostDetails(this.paramId).subscribe({
+        next: (post) => {
+          this.currPost = post;
+        },
+        error: (err) => {
+          alert(err.error.errors.join('\n'));
+          console.error(err); 
+        }
+      });
     });
+  }
+
+  formSubmit(form: NgForm){
+    if (form.invalid) {
+      return alert('invalid form');
+    }
+    this.isLoading = true;
+    this.sPost.editPost(this.paramId, form.value).subscribe({
+      next: () => {
+        this.router.navigate(['/post/'+this.paramId])
+      },
+      error: (err) => {
+        alert(err.error.errors.join('\n'));
+        console.error(err); 
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    this.authDataSubscription?.unsubscribe();
   }
 }
